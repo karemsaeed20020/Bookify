aee4250f0530fc6c69b732fe847b268818c09d76
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-11-20 08:32:22 +0200

    Add project files.

---
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,95 @@
+﻿using Bookify.Web.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.AsNoTracking().ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return PartialView("_Form");
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("_Form",model);
+            }
+            var category = new Category
+            {
+                Name = model.Name
+            };
+            _context.Add(category);
+            _context.SaveChanges();
+            TempData["Message"] = "Saved Successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+            var viewModel = new CategoryFormViewModel
+            {
+                Id = id,
+                Name = category.Name
+            };
+            return View("_Form", viewModel);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(CategoryFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("_Form", model);
+            }
+            var category = _context.Categories.Find(model.Id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+           category.Name = model.Name;
+            category.LastUpdatedOn = DateTime.Now;
+            _context.SaveChanges();
+            TempData["Message"] = "Saved Successfully";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult ToggleStatus(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            category.IsDeleted = !category.IsDeleted;
+            category.LastUpdatedOn = DateTime.Now;
+            _context.SaveChanges();
+
+            // Return the new updated time so JS can update UI
+            return Ok(category.LastUpdatedOn.ToString());
+        }
+
+    }
+}
---
+++ Bookify.Web/wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿function showSuccessMessage(message = 'Saved Successfully!') {
+    Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: message,
+        customClass: {
+            confirmButton: 'btn btn-outline btn-outline-dashed btn-outline-primary btn-active-light-primary'
+        }
+    })
+}
+
+function showErrorMessage(message = 'Something went wrong!') {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...!',
+        text: message,
+        customClass: {
+            confirmButton: 'btn btn-outline btn-outline-dashed btn-outline-primary btn-active-light-primary' // Fixed: removed nested object
+        }
+    })
+}
+$(document).ready(function () {
+    var message = $('#Message').text();
+    if (message !== '') {
+        showSuccessMessage(message);
+    }
+    // Handle bootstrap Modal
+    $('.js-render-modal').on('click', function () {
+        var modal = $('#Modal');
+        var btn = $(this);
+        modal.find('#ModalLabel').text(btn.data('title'));
+        $.get({
+            url: btn.data('url'),
+            success: function (form) {
+                modal.find('.modal-body').html(form);
+                $.validator.unobtrusive.parse(modal);  
+            },
+            error: function () {
+                showErrorMessage();
+            }
+        })
+        modal.modal('show');
+
+    })
+})
