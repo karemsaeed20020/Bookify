e3786607d1b00b7e39aada6b53bc27e94a31b61c
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2025-11-25 20:27:43 +0200

    Add Create Get Action

--- Bookify.Web/Controllers/BooksController.cs
+++ Bookify.Web/Controllers/BooksController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Bookify.Web.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public BooksController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Create()
         {
-            return View("Form");  
+            var authors = _context.Authors.Where(a => !a.IsDeleted).OrderBy(a => a.Name).ToList();
+            var categories = _context.Categories.Where(a => !a.IsDeleted).OrderBy(a => a.Name).ToList();
+            var viewModel = new BookFromViewModel
+            {
+                Authors = _mapper.Map<IEnumerable<SelectList>>(authors),
+                Categories = _mapper.Map<IEnumerable<SelectList>>(categories)
+            };
+            return View("Form", viewModel);  
         }
     }
 }
