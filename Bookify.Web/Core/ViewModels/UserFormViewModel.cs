eb9dee181f8eec61291565763cb73c1ee91b7405
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2026-01-01 23:12:17 +0200

    Add User Form ViewModel

---
+++ Bookify.Web/Core/ViewModels/UserFormViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Bookify.Web.Core.Consts;
+using ExpressiveAnnotations.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookify.Web.Core.ViewModels
+{
+    public class UserFormViewModel
+    {
+        public string? Id { get; set; }
+
+        [MaxLength(100, ErrorMessage = Errors.MaxLength), Display(Name = "Full Name")]
+        public string FullName { get; set; } = null!;
+
+        [MaxLength(20, ErrorMessage = Errors.MaxLength), Display(Name = "Username")]
+        [Remote("AllowUserName", null!, AdditionalFields = "Id", ErrorMessage = Errors.Duplicated)]
+        public string UserName { get; set; } = null!;
+
+        [MaxLength(200, ErrorMessage = Errors.MaxLength), EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [DataType(DataType.Password),
+            StringLength(100, ErrorMessage = Errors.MaxMinLength, MinimumLength = 8)]
+        public string? Password { get; set; } = null!;
+
+        [DataType(DataType.Password), Display(Name = "Confirm password"),
+            Compare("Password", ErrorMessage = Errors.ConfirmPasswordNotMatch)]
+        public string? ConfirmPassword { get; set; } = null!;
+
+        [Display(Name = "Roles")]
+        public IList<string> SelectedRoles { get; set; } = new List<string>();
+
+        public IEnumerable<SelectListItem>? Roles { get; set; }
+    }
+}
--- Bookify.Web/Core/ViewModels/UserViewModel.cs
+++ Bookify.Web/Core/ViewModels/UserViewModel.cs
@@ -8,6 +8,6 @@
         public string Email { get; set; } = null!;
         public bool IsDeleted { get; set; }
         public DateTime CreatedOn { get; set; }
-        public DateTime LastUpdatedOn { get; set; }
+        public DateTime? LastUpdatedOn { get; set; }
     }
 }
