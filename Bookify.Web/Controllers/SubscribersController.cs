b1adb7cb2d22bf82520f6be6ffe2d638b9e89fec
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2026-04-06 21:21:35 +0200

    Add subscriber management feature with UI and validation
    Introduced support for managing subscribers, including:
    - New SubscribersController with list, create, and AJAX area loading
    - Models for Subscriber, Governorate, and Area with EF Core indexes
    - View models and AutoMapper profiles for forms and display
    - Validation constants and regex for national ID and images
    - Razor views for search and create/edit forms
    - JavaScript for dynamic area dropdowns
    - Sidebar menu updates based on user roles

---
+++ Bookify.Web/Controllers/SubscribersController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Bookify.Web.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bookify.Web.Controllers
+{
+    public class SubscribersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public SubscribersController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            var viewModel = new SubscriberFormViewModel
+            {
+                Governorates = _context.Governorates.Select(g => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+                {
+                    Value = g.Id.ToString(),
+                    Text = g.Name
+                })
+                .OrderBy(g => g.Text)
+                .ToList(),
+            };
+            return View("Form", viewModel);
+        }
+
+        [AjaxOnly]
+        public IActionResult GetAreas(int governorateId)
+        {
+            var areas = _context.Areas
+                    .Where(a => a.GovernorateId == governorateId && !a.IsDeleted)
+                    .OrderBy(g => g.Name)
+                    .ToList();
+
+            return Ok(_mapper.Map<IEnumerable<SelectListItem>>(areas));
+        }
+
+
+
+
+    }
+}
--- Bookify.Web/Core/Mapping/MappingProfile.cs
+++ Bookify.Web/Core/Mapping/MappingProfile.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using Bookify.Web.Core.Models;
-using Bookify.Web.Core.ViewModels;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Bookify.Web.Core.Mapping
@@ -44,6 +43,24 @@ namespace Bookify.Web.Core.Mapping
                 .ForMember(dest => dest.NormalizedEmail, opt => opt.MapFrom(src => src.Email.ToUpper()))
                 .ForMember(dest => dest.NormalizedUserName, opt => opt.MapFrom(src => src.UserName.ToUpper()))
                 .ReverseMap();
+            //Governorates & Areas
+            CreateMap<Governorate, SelectListItem>()
+                .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<Area, SelectListItem>()
+                .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => src.Name));
+
+            //Subscribers
+            CreateMap<Subscriber, SubscriberFormViewModel>().ReverseMap();
+
+
+            CreateMap<Subscriber, SubscriberViewModel>()
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(dest => dest.Area, opt => opt.MapFrom(src => src.Area!.Name))
+                .ForMember(dest => dest.Governorate, opt => opt.MapFrom(src => src.Governorate!.Name));
+
 
 
         }
